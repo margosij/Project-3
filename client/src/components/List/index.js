d2c603b4870d23cb5aca06b6e55e30f71b84b6dd
armonkahil <armonkahil@gmail.com>
2020-01-15 00:26:59 -0500

    added components, added boilerplates for different pages, added react router, added spinner component

---
+++ client/src/components/List/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function List({ children }) {
+  return <ul className='list-unstyled col-8 mx-auto'>{children}</ul>
+}
+
+export function ListItem({ children }) {
+  return <li className='list-group-item'>{children}</li>
+}
