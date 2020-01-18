63d0f2f612383ba98cf6d914e7491efee53c5be9
Kimberlyn Creary <kimocreary@gmail.com>
2020-01-18 14:54:21 -0500

    find user updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,52 +7,39 @@ module.exports = {
     createUser: (req, res) => {
       let newuser = req.body
         newuser.password = bcrypt.hashSync(newuser.password, bcrypt.genSaltSync(10));
-
-    
-
         db.User.create(newuser)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err))
     },
     logInUser: (req, res) => {
-        res.send('Log In Route Was Hit')
+        db.User.findOne({username : req.body.username})
+        .then(
+            (dbModel)=>{
+                if (!dbModel) {
+                    res.send('User not found')
+                }
+                if (bcrypt.compareSync(req.body.password, dbModel.password)) {
+                    res.send('User logged in')
+                }else {
+                    res.send('Not logged in')
+                }
+
+            }
+        )
+        .catch(err => res.status(422).json(err))
+
+
+
+
+
+
+
+
+        // res.send('Log In Route Was Hit')
     },
     findAllUsers: (req, res) => {
         db.User.find({})
             .then(dbModel => res.json(dbModel))
             .catch(error => res.status(422).json(error))
     },
-
-  // find all Admin
-  findAllAdmins: (req, res) => {
-    db.Admin.find(req.query)
-      .sort('lastName -test')
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err))
-  },
-  // find an Admin by id
-  findAdminById: (req, res) => {
-    db.Admin.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err))
-  },
-  // create an Admin
-  createAdmin: (req, res) => {
-    db.Admin.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err))
-  },
-  // update an Admin
-  updateAdmin: (req, res) => {
-    db.Admin.findOneAndUpdate({ _id: req.body._id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err))
-  },
-  // remove an Admin
-  removeAdmin: (req, res) => {
-    db.Admin.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err))
-  }
 }
