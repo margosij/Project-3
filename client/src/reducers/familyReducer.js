5a6c22c86f075c662ceb6e1612920a2d0d799c6e
Noah King <noahking21@gmail.com>
2020-01-27 19:34:02 -0500

    waitlist redux

--- client/src/reducers/familyReducer.js
+++ client/src/reducers/familyReducer.js
@@ -3,39 +3,49 @@ import {
   GET_FAMILIES,
   FAMILY_LOADING,
   CLEAR_CURRENT_FAMILY,
-} from '../actions/types'
+  ADD_FAMILY_TO_WAITLIST
+} from "../actions/types";
 
 const initialState = {
   family: null,
   families: null,
-  loading: false
-}
+  loading: false,
+  waitlist: []
+};
 
 export default (state = initialState, action) => {
+  console.log("action:", action.payload);
   switch (action.type) {
     case FAMILY_LOADING:
       return {
         ...state,
         loading: true
-      }
+      };
     case GET_FAMILY:
       return {
         ...state,
         family: action.payload,
         loading: false
-      }
+      };
     case GET_FAMILIES:
       return {
         ...state,
         families: action.payload,
         loading: false
-      }
+      };
     case CLEAR_CURRENT_FAMILY:
       return {
         ...state,
         family: null
-      }
+      };
+    case ADD_FAMILY_TO_WAITLIST:
+      return {
+        ...state,
+        waitlist: state.waitlist.includes(action.payload)
+          ? state.waitlist
+          : [...state.waitlist, action.payload]
+      };
     default:
-      return state
+      return state;
   }
-}
+};
