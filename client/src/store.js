f0a246da8dd3c21bb57d8955a9d4ed41215d126f
armonkahil <armonkahil@gmail.com>
2020-02-24 18:07:27 -0500

    added more devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 const initialState = {}
 // thunk is a middle ware that makes calls asynchronous
 const middleware = [thunk]
-
+console.log(process.env)
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
+  composeWithDevTools(applyMiddleware(...middleware)
     // this is for the Redux devtools Chrome Extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 const db = require('../models')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/dismissal'
-console.log('MONGODB_URI:', MONGODB_URI)
 const bcrypt = require('bcryptjs')
 const gravatar = require('gravatar')
 mongoose.Promise = global.Promise
-//  console.log('global:', global.Promise)
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true })
